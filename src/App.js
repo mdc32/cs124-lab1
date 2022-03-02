b1ad4f4c75c3101b31c5cb80cc985de66228c28d
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2022-03-01 21:55:16 -0800

    added most handlers to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import React from 'react';
 import Header from './Header';
 import ListContainer from './ListContainer'
@@ -6,14 +6,44 @@ import {useEffect, useState} from 'react';
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 
 function App(props) {
-  const [toggleHideCompleted, setToggleHideCompleted] = useState(false)
-  const [toggleTrash, setToggleTrash] = useState(false)
-  const [listItems, setListItems] = useState([])
+
+  const [isShowCompleted, setIsShowCompleted] = useState(true)
+  const [tasks, setTasks] = useState(props.data)
+  // [{id: 123, text: "call mom", isCompleted: true}]
+
+  const handleToggleShowCompleted = () => setIsShowCompleted(!isShowCompleted)
+
+  function handleDeleteCompleted() {
+    setTasks(tasks.filter(t => !t.isCompleted))
+  };
+
+  function handleChangeField(id, field, value) {
+    setTasks(tasks.map(
+      t => t.id === id ? {...t, [field]: value} : t))
+  }
+
+  function handleToggleItemCompleted(id) {
+    handleChangeField(id, "isCompleted", !tasks.find(t => t.id === id).isCompleted)
+  }
+
+  function handleAddNewTask() {
+    setTasks([...tasks,
+      {
+        id: generateUniqueID(),
+        text: "",
+        isCompleted: false
+      }
+    ]);  
+  }
+
 
   return (
     <div className="App">
-      <Header></Header>
-      <ListContainer items={props.data}></ListContainer>
+      <Header
+        onToggleShowCompleted={handleToggleShowCompleted}
+      ></Header>
+      <ListContainer
+        items={tasks.filter(t => !t.isCompleted || isShowCompleted)}></ListContainer>
     </div>
   );
 }
