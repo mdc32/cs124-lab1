8fdd44ffaa75b52341212e3b0e231fe17093c30a
Xoaquin <xbaca@g.hmc.edu>
2022-03-01 21:21:37 -0800

    Added States

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import {useEffect, useState} from 'react';
+import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 
 function App(props) {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    const [toggleHideCompleted, setToggleHideCompleted] = useState(false)
+    const [toggleTrash, setToggleTrash] = useState(false)
+    return (
+        <div className="App">
+            <Header></Header>
+        </div>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,17 +2,15 @@ import './Header.css'
 import { FaEye, FaTrash } from 'react-icons/fa'
 
 function Header(props) {
-    return (
-        <div id="header">
+    return <div id="header">
             <div class="icon">
                 <FaEye/>
             </div>
             <h3>Tasks</h3>
             <div class="icon ">
-                <FaTrash>
+                <FaTrash/>
             </div>
-        </div>
-    )
+            </div>
 }
 
 export default Header;
